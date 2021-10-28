d10d32c560575b0296bcee2306c84bde4562df98
ParkJiHo <iks15174@daum.net>
2021-10-29 00:50:18 +0900

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
-// import React from 'react';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 
-// import HeroList from './containers/HeroList/HeroList';
-// import RealDetail from './containers/HeroList/RealDetail/RealDetail';
-// import NewHero from './containers/HeroList/NewHero/NewHero';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-// import { Route, Redirect, Switch } from 'react-router-dom';
-// import { ConnectedRouter } from 'connected-react-router';
-
-// function App(props) {
-//   return (
-//     <ConnectedRouter history={props.history}>
-//       <div className="App" >
-//         <Switch>
-//           <Route path='/heros' exact render={() => <HeroList title="MY FAVORITE HEROS" />} />
-//           <Route path='/heros/:id' exact component={RealDetail} />
-//           <Route path='/new-hero' exact component={NewHero} />
-//           <Redirect exact from='/' to='heros' />
-//           <Route render={() => <h1>Not Found</h1>} />
-//         </Switch>
-//       </div >
-//     </ConnectedRouter>
-//   );
-// }
-
-// export default App;
+export default App;
