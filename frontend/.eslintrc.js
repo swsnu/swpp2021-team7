79049d2207c9be0412804451673f3c78186378a2
ParkJiHo <iks15174@daum.net>
2021-10-28 22:41:47 +0900

    travis.yml 파일 추가

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+        jest: true
+    },
+    "extends": [
+        'plugin:react/recommended',
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
