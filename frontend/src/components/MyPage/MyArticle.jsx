f8e3c82e65c3326a5751a3cfaf3948d923343c36
ParkJiHo <iks15174@daum.net>
2021-11-08 12:37:04 +0900

    MyPage My Article UI update

--- frontend/src/components/MyPage/MyArticle.jsx
+++ frontend/src/components/MyPage/MyArticle.jsx
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import {ListItem, Button, Divider, ListItemText, Typography} from '@mui/material';
+import {
+    ListItem,
+    Button,
+    Divider,
+    ListItemText,
+    Typography,
+    Chip,
+    ListItemButton
+} from '@mui/material';
 
 
 export default class MyArticle extends Component {
@@ -10,23 +18,12 @@ export default class MyArticle extends Component {
         return (
             <React.Fragment>
                 <ListItem>
-                    <ListItemText
-                        sx={{ display: 'block'}}
-                        primary={<a onClick={this.onClickScrapedArticle}>{"Title : Scrap1"}</a>}
-                        secondary={
-                            <React.Fragment>
-                                <Typography
-                                    sx={{ display: 'inline' }}
-                                    component="span"
-                                    variant="body2"
-                                    color="text.primary"
-                                >
-                                    {"link : https://miro.com/app/board/o9J_lsOtDfA=/"}
-                                </Typography>
-                            </React.Fragment>
-                        }
-                    />
-                    <Button variant="contained" onClick={()=>this.deleteScrapedArticle()}>delete</Button>
+                    <ListItemButton component="a" href="/">
+                        <ListItemText
+                            primary='타블로와 슬기, 빅나티, 노보가 함께한 라코스테 X 피너츠 협업 캠페인'
+                        />
+                    </ListItemButton>
+                    <Button variant="contained" onClick={() => this.deleteScrapedArticle()}>delete</Button>
                 </ListItem>
                 <Divider variant="inset" component="li" />
             </React.Fragment>
