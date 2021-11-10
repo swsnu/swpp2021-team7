ef4cf765dd97b78c49918ec50ee7f1245a99eb50
Sohyun Kim <prrsh22@gmail.com>
2021-11-11 01:36:35 +0900

    set dummy data aside

---
+++ frontend/src/constants.js
@@ -0,0 +1,55 @@
+export const groupDummy = {
+    basicInfo: {
+        image: 'https://pbs.twimg.com/media/E85o_8MVgAM58Gd.jpg',
+        info: {name: {kor: '레드벨벳', eng: 'Red Velvet'}, group: 'Red Velvet', birth: '1994.02.10', debut: '2014.08.01'},
+        news: [ {title: '레드벨벳 조이 \'사랑스런 가을여인\'[영상]', url: 'https://entertain.naver.com/read?oid=108&aid=0003003508'},
+                {title: '레드벨벳 웬디였어? 실감나는 연기, 사랑스러운 매력', url: 'http://entertain.naver.com/read?oid=015&aid=0004625540'},
+                {title: '레드벨벳 예리, 마스크도 명품으로..심장 녹이는 윙크', url: 'https://entertain.naver.com/read?oid=112&aid=0003496002'}]
+    },
+    tweets: [{author: 'bae', content: 'my wishlist for red velvet:\n- #YERI, #SEULGI solo debut\n- #WENDY rnb album \n- #JOY original album\n- wendy ost for a popular kdrama\n- US label!!!!\n- group ost\n- #IRENE kdrama, cfs, mag shoots, brand deals\n- group mag shoot\n- killing voice, it\'s live guesting\n- dance practice vidsd', avatar: '/static/images/avatar/1.jpg'},
+            {author: 'rvchart', content: "@RVsmtown's Bad Boy re-entered Billboard World Digital Song Sales chart for the week of November 13, 2021 Party popper", avatar: "/static/images/avatar/2.jpg", image: "https://pbs.twimg.com/media/FDywImZUUAQfO8X?format=jpg&name=900x900"},
+            {author: "dip✨", content: "throwback to the one of the best concert in k-pop history... la rouge concert was for the history books @RVsmtown", avatar: "/static/images/avatar/3.jpg", image: "https://pbs.twimg.com/media/FD0Mw8eVcAAhNIn?format=jpg&name=medium"}],
+    youtubes: [
+        {uploadDate: '5 days ago', title: "2022 Red Velvet SEASON’S GREETINGS CONCEPT TRAILER #RedVelvet", thumbnail: 'https://i.ytimg.com/vi/cTjSbnA4WhI/hq2.jpg', url: 'https://youtube.com/shorts/cTjSbnA4WhI?feature=share'},
+        {uploadDate: '1 month ago', title: "Red Velvet 레드벨벳 'Queendom' Compilation", thumbnail: 'https://i.ytimg.com/vi/i8wv1PWGFoo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCO8nmPu2Nwrt6IvF_cvt9ws2kl2w', url: 'https://youtu.be/i8wv1PWGFoo'},
+        {uploadDate: '2 months ago', title: "Red Velvet 레드벨벳 '다시, 여름 (Hello, Sunset)' Special VideoㅣRV Collection", thumbnail: "https://i.ytimg.com/vi/DDokLv2ujyk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCfAQ0rcg8KK4im3SY8Ok_jdwIoWgz", url: "https://youtu.be/DDokLv2ujyk"},
+    ],
+    shared: [
+        {uploader: 'seulseul', title: "[STATION X 0] 슬기(SEULGI)X신비(여자친구)X청하X소연 'Wow Thing' MV", thumbnail: 'https://i.ytimg.com/vi/KR5CtMLuiqQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC_9ZgdbeD0mNkN1VGoG2Vzd3U8IQ', url: 'https://youtu.be/KR5CtMLuiqQ'},
+        {uploader: 'kang', title: "Red Velvet 레드벨벳 'Queendom' MV BehindㅣRV Collection", thumbnail: 'https://i.ytimg.com/vi/nbfJU8WkcO4/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDZJAE8Uq5Aae-pk352Ga7KBYXoBg', url: 'https://youtu.be/KR5CtMLuiqQ'},
+        {uploader: 'rvfanboy', title: "Red Velvet 레드벨벳 'Queendom' MV", thumbnail: 'https://i.ytimg.com/vi/c9RzZpV460k/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAkifyd7qlRi1nUBuDnELaT-rxnLQ', url: 'https://youtu.be/KR5CtMLuiqQ'},
+    ],
+    comments: [
+        {author: "seulgijjang", content: "will you guys buy the season greeting?", timestamp: "3 days ago"},
+        {author: "cheodory", content: "sure;;", timestamp: "23 hours ago"},
+        {author: "hihi", content: "i wanna go to the concert", timestamp: "1 hour ago"},
+    ]
+}
+
+export const memberDummy = {
+    basicInfo: {
+        image: 'https://img.insight.co.kr/static/2019/01/31/700/18nd52ajs5z4750u7p6f.jpg',
+        info: {name: {kor: '강슬기', eng: 'Seulgi'}, group: 'Red Velvet', birth: '1994.02.10', debut: '2014.08.01'},
+        news: [ {title: '타블로와 슬기, 빅나티, 노보가 함께한 라코스테 X 피너츠 협업 캠페인', url: 'https://www.gqkorea.co.kr/2021/11/05/%ED%83%80%EB%B8%94%EB%A1%9C%EC%99%80-%EC%8A%AC%EA%B8%B0-%EB%B9%85%EB%82%98%ED%8B%B0-%EB%85%B8%EB%B3%B4%EA%B0%80-%ED%95%A8%EA%BB%98%ED%95%9C-%EB%9D%BC%EC%BD%94%EC%8A%A4%ED%85%8C-x-%ED%94%BC%EB%84%88/'},
+                {title: '레드벨벳 슬기, 러블리한 매력 폭발..인형이라 해도 믿겠어♥', url: 'https://entertain.naver.com/read?oid=112&aid=0003493956'},
+                {title: '아이린x슬기x예리, 방구석 핼러윈 파티 인증샷 공개', url: 'https://entertain.naver.com/read?oid=009&aid=0004872615'}]
+    },
+    tweets: [{author: 'gomseulgi', content: 'Seulgi is god', avatar: '/static/images/avatar/1.jpg'},
+            {author: '휴먼히읗', content: "FINNALY TAEYONG AND SEULGI SINGING ROSE LIVE 😭😭", avatar: "/static/images/avatar/2.jpg"},
+            {author: "슬기.zip", content: "#슬기zip 완벽 재현📸 2021 ver. 프링글슬기👶🏻 쥔님 방부제 미모 어떡해😆 옛날도 지금도 변함 없는 사실❗️#슬기 는 세상에서 제일 귀여운 존재💞 #NOW온에어 화요일 투슬✌🏻레전드 발라더 #2am 전격 방문🏡 감성 가득 슬기zip 절대 본방사수👀", avatar: "/static/images/avatar/3.jpg", image: "https://pbs.twimg.com/media/FDWz92hakAEnzFV?format=jpg&name=4096x4096"}],
+    youtubes: [
+        {uploadDate: '5 days ago', title: "2022 Red Velvet SEASON’S GREETINGS CONCEPT TRAILER #RedVelvet", thumbnail: 'https://i.ytimg.com/vi/cTjSbnA4WhI/hq2.jpg', url: 'https://youtube.com/shorts/cTjSbnA4WhI?feature=share'},
+        {uploadDate: '1 month ago', title: "Red Velvet 레드벨벳 'Queendom' Compilation", thumbnail: 'https://i.ytimg.com/vi/i8wv1PWGFoo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCO8nmPu2Nwrt6IvF_cvt9ws2kl2w', url: 'https://youtu.be/i8wv1PWGFoo'},
+        {uploadDate: '2 months ago', title: "Red Velvet 레드벨벳 '다시, 여름 (Hello, Sunset)' Special VideoㅣRV Collection", thumbnail: "https://i.ytimg.com/vi/DDokLv2ujyk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCfAQ0rcg8KK4im3SY8Ok_jdwIoWgz", url: "https://youtu.be/DDokLv2ujyk"},
+    ],
+    shared: [
+        {uploader: 'seulseul', title: "[STATION X 0] 슬기(SEULGI)X신비(여자친구)X청하X소연 'Wow Thing' MV", thumbnail: 'https://i.ytimg.com/vi/KR5CtMLuiqQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC_9ZgdbeD0mNkN1VGoG2Vzd3U8IQ', url: 'https://youtu.be/KR5CtMLuiqQ'},
+        {uploader: 'kang', title: "Red Velvet 레드벨벳 'Queendom' MV BehindㅣRV Collection", thumbnail: 'https://i.ytimg.com/vi/nbfJU8WkcO4/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDZJAE8Uq5Aae-pk352Ga7KBYXoBg', url: 'https://youtu.be/KR5CtMLuiqQ'},
+        {uploader: 'rvfanboy', title: "Red Velvet 레드벨벳 'Queendom' MV", thumbnail: 'https://i.ytimg.com/vi/c9RzZpV460k/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAkifyd7qlRi1nUBuDnELaT-rxnLQ', url: 'https://youtu.be/KR5CtMLuiqQ'},
+    ],
+    comments: [
+        {author: "seulgijjang", content: "will you guys buy the season greeting?", timestamp: "3 days ago"},
+        {author: "cheodory", content: "sure;;", timestamp: "23 hours ago"},
+        {author: "hihi", content: "i wanna go to the concert", timestamp: "1 hour ago"},
+    ]
+}
