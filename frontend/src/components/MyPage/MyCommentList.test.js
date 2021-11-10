e185959addfc7377c3752cb826b503330e5497a1
ParkJiHo <iks15174@daum.net>
2021-11-10 12:23:50 +0900

    Create MyPage components test code

---
+++ frontend/src/components/MyPage/MyCommentList.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';;
+import { getMockStore } from '../../test-utils/mocks';
+import { history } from '../../store/store';
+import { ConnectedRouter } from 'connected-react-router';
+import MyCommentList from './MyCommentList';
+
+
+
+const mockStore = getMockStore({});
+
+describe('<MyCommentList />', () => {
+    let component = null
+    let setComponent = () => {
+        component = mount(
+            <Provider store={mockStore} >
+                <ConnectedRouter history={history}>
+                    <MyCommentList></MyCommentList>
+                </ConnectedRouter>
+            </Provider>
+        )
+    }
+    it('should render without errors', () => {
+        setComponent()
+        const listItem = component.find('ForwardRef(List)')
+        expect(listItem.length).toBe(1);
+    })
+
+})
