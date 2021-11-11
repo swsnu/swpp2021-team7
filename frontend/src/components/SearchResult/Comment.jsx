4c411d47d48b93d623f4492ded5c2af65ae94022
Sohyun Kim <prrsh22@gmail.com>
2021-11-11 13:32:43 +0900

    implement comment like

--- frontend/src/components/SearchResult/Comment.jsx
+++ frontend/src/components/SearchResult/Comment.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Chip, ListItem, ListItemText, } from "@mui/material";
 
 const Comment = ({author, content, timestamp}) => {
 
+    const [likeClicked, setLikeClicked] = useState(false);
+
     return <ListItem>
             <Chip label={author} color="primary" />
             <ListItemText primary={content} style={{marginLeft: "10px"}} />
-            <Chip label="❤️ Like" style={{marginRight: "10px"}} />
+            {likeClicked ? <Chip label="❤️ Like" style={{marginRight: "10px"}} color="primary" onClick={() => setLikeClicked(!likeClicked)}/> : 
+                <Chip label="❤️ Like" style={{marginRight: "10px"}} color="primary" variant="outlined" onClick={() => setLikeClicked(!likeClicked)}/>}
             <Chip label={timestamp}/>
     {/* <Button variant="contained" onClick={() => alert("Successfully scraped!")}>수정삭제등등..</Button> */}
 </ListItem>
