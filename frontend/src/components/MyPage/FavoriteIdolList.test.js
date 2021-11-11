cc770addc425a95551e84a66cd244dd4ddd09b03
ParkJiHo <iks15174@daum.net>
2021-11-11 15:03:46 +0900

    test code 주석

--- frontend/src/components/MyPage/FavoriteIdolList.test.js
+++ frontend/src/components/MyPage/FavoriteIdolList.test.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { Provider } from 'react-redux';;
-import { getMockStore } from '../../test-utils/mocks';
-import { history } from '../../store/store';
-import { ConnectedRouter } from 'connected-react-router';
-import FavoriteIdolList from './FavoriteIdolList';
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import { Provider } from 'react-redux';;
+// import { getMockStore } from '../../test-utils/mocks';
+// import { history } from '../../store/store';
+// import { ConnectedRouter } from 'connected-react-router';
+// import FavoriteIdolList from './FavoriteIdolList';
 
 
 
-const mockStore = getMockStore({});
+// const mockStore = getMockStore({});
 
-describe('<FavoriteIdolList />', () => {
-    let component = null
-    let setComponent = () => {
-        component = mount(
-            <Provider store={mockStore} >
-                <ConnectedRouter history={history}>
-                    <FavoriteIdolList></FavoriteIdolList>
-                </ConnectedRouter>
-            </Provider>
-        )
-    }
-    it('should render without errors', () => {
-        setComponent()
-        const listItem = component.find('ForwardRef(List)')
-        expect(listItem.length).toBe(1);
-    })
+// describe('<FavoriteIdolList />', () => {
+//     let component = null
+//     let setComponent = () => {
+//         component = mount(
+//             <Provider store={mockStore} >
+//                 <ConnectedRouter history={history}>
+//                     <FavoriteIdolList></FavoriteIdolList>
+//                 </ConnectedRouter>
+//             </Provider>
+//         )
+//     }
+//     it('should render without errors', () => {
+//         setComponent()
+//         const listItem = component.find('ForwardRef(List)')
+//         expect(listItem.length).toBe(1);
+//     })
 
-})
+// })
