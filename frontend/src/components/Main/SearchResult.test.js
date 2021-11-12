43fb21c5d113df4c47d221619c5132f001b38f98
eunbin1227 <eunbin1227@snu.ac.kr>
2021-11-13 04:25:33 +0900

    Main Component test

---
+++ frontend/src/components/Main/SearchResult.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';;
+import { getMockStore } from '../../test-utils/mocks';
+import { history } from '../../store/store';
+import { ConnectedRouter } from 'connected-react-router';
+import SearchResult from './SearchResult';
+
+const mockStore = getMockStore({});
+const name = '테스트 Test'
+
+describe('<SearchResult />', () => {
+    let component = null;
+    let setComponent = () => {
+        component = mount(
+            <Provider store={mockStore} >
+                <ConnectedRouter history={history}>
+                    <SearchResult name={name}></SearchResult>
+                </ConnectedRouter>
+            </Provider>
+        )
+    };
+
+    it('should render without errors', () => {
+        setComponent()
+        const listItem = component.find('ForwardRef(ListItem)')
+        const listItemTexts = component.find('ForwardRef(ListItemText)')
+        
+        expect(listItem.length).toBe(1);
+        expect(listItemTexts.at(0).text()).toContain(name)
+    })
+
+    it('should redirect to Search Result Page', () => {
+        const spyHistory = jest.spyOn(history, 'push')
+
+        let idolInfos = component.find('ForwardRef(Chip)')
+        expect(idolInfos.length).toBe(1)
+
+        idolInfos.at(0).simulate('click')
+        expect(spyHistory).toBeCalledWith('/search/group/1')
+
+        spyHistory.mockRestore()
+    })
+})
