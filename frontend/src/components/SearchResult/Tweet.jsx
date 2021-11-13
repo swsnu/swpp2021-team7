00d38f2c722e23102146fcf48accc77f148d5d5c
Sohyun Kim <prrsh22@gmail.com>
2021-11-13 16:00:30 +0900

    Add test for `Tweet`

--- frontend/src/components/SearchResult/Tweet.jsx
+++ frontend/src/components/SearchResult/Tweet.jsx
@@ -7,49 +7,52 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { Backdrop } from '@mui/material';
 
-export default function Tweet ({avatar, author, content, image}) {
+export default function Tweet ({avatar, author, content, image, close, toggle}) {
 
     const [open, setOpen] = React.useState(false);
     
     const handleClose = () => {
-      setOpen(false);
+      if (close) close();
+      else setOpen(false);
     };
 
     const handleToggle = () => {
-      setOpen(!open);
+      if (toggle) toggle();
+      else setOpen(!open);
     };
 
-    return <div>
-    <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src={avatar} />
-        </ListItemAvatar>
-        <ListItemText
-        primary={content}
-        secondary={
-            <React.Fragment>
-            <Typography
-                sx={{ display: 'inline' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-            >
-                - {author}
-            </Typography>
-            </React.Fragment>
-        }
-        />
-        {image && <>
-            <img width="20%" onClick={handleToggle} src={image} />
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={open}
-                onClick={handleClose}
-            >
-            <img height="90%" onClick={handleToggle} src={image} />
-            </Backdrop>
-        </>}
-    </ListItem>
-    <Divider variant="inset" component="li" />
+    return <div className="tweet">
+        <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src={avatar} />
+            </ListItemAvatar>
+            <ListItemText
+            primary={content}
+            secondary={
+                <React.Fragment>
+                <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                >
+                    - {author}
+                </Typography>
+                </React.Fragment>
+            }
+            />
+            {image && <>
+                <img width="20%" onClick={handleToggle} src={image} className="image-before-click" />
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={open}
+                    onClick={handleClose}
+                    className="backdrop"
+                >
+                <img height="90%" onClick={handleToggle} src={image} className="image-after-click"/>
+                </Backdrop>
+            </>}
+        </ListItem>
+        <Divider variant="inset" component="li" />
   </div>
 }
