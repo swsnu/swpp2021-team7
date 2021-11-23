bd9a1ef55e305bb8a3f1a90e8a05b89baced2978
ParkJiHo <iks15174@daum.net>
2021-11-23 17:54:01 +0900

    create IdolMemberImage model

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class SearchLog(models.Model):
     query = models.TextField(blank=False, default="")
     isMember = models.BooleanField(default=False)
@@ -13,7 +12,8 @@ class ImageResource(models.Model):
     address = models.TextField(blank=False, default="")
     size = models.IntegerField(default=0)
     time = models.DateTimeField(auto_now_add=True)
-
+    
+    
 
 class VideoResource(models.Model):
     videoType = models.IntegerField(default=0)
