0d4c3024f8192f1f8b751c73263a846663ceb1e6
Sohyun Kim <prrsh22@gmail.com>
2021-11-27 14:14:15 +0900

    Implement `crawl_member` in `crawl_basic_info`

--- backend/search_result/management/commands/crawl_basic_info.py
+++ backend/search_result/management/commands/crawl_basic_info.py
@@ -18,9 +18,32 @@ class Command(BaseCommand):
         self.iterate()
 
     def iterate(self):
-        to_crawl = ["레드벨벳", "방탄소년단"]
+        to_crawl = ["레드벨벳", "방탄소년단", "비투비"]
         for group in to_crawl:
             self.crawl_group(group)
+
+    @staticmethod
+    def fetch_and_set(url):
+        html = BeautifulSoup(
+            requests.get(url).text,
+            "html.parser",
+        )
+
+        content_area = html.select_one("section._au_people_content_wrap")
+        name_eng = content_area.select_one("div.title_area div.sub_title span.txt").text
+
+        if "가수" in name_eng:
+            name_eng = ""
+
+        info_nodes = content_area.select("dl.info div.info_group")
+        if len(content_area.select("div.detail_info img")):
+            thumbnail_address = content_area.select_one("div.detail_info img")["src"]
+        else:
+            thumbnail_address = content_area.select_one("div.img_scroll div.thumb img")[
+                "src"
+            ]
+
+        return name_eng, info_nodes, thumbnail_address
 
     def crawl_group(self, group_kor):
 
@@ -34,20 +57,11 @@ class Command(BaseCommand):
 
         url = f"https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query={group_kor}"
 
-        html = BeautifulSoup(
-            requests.get(url).text,
-            "html.parser",
-        )
-
-        content_area = html.select_one("section._au_people_content_wrap")
-        group_eng = content_area.select_one(
-            "div.title_area div.sub_title span.txt"
-        ).text
+        group_eng, info_nodes, thumbnail_address = self.fetch_and_set(url)
 
         name = {"kor": group_kor, "eng": group_eng}
         group_instance = IdolGroup.objects.create(name=name)
 
-        info_nodes = content_area.select("dl.info div.info_group")
         info = {}
         source = {}
 
@@ -79,8 +93,6 @@ class Command(BaseCommand):
 
             info[key] = value
 
-        thumbnail_address = html.select_one("div.detail_info img")["src"]
-
         image_resource_instance = ImageResource.objects.create(
             address=thumbnail_address
         )
@@ -92,7 +104,52 @@ class Command(BaseCommand):
         )
 
         for member in member_urls:
-            self.crawl_member(member)
+            self.crawl_member(group_instance.id, member)
 
-    def crawl_member(self, member_tuple):
-        pass
+    def crawl_member(self, group_id, member_tuple):
+
+        member_kor, url = member_tuple
+        print(f"Crawling {member_kor}(member)...")
+
+        member_eng, info_nodes, thumbnail_address = self.fetch_and_set(url)
+
+        instance_exists = (
+            IdolMemberIncluded.objects.filter(group__id=group_id)
+            .filter(member__name__kor=member_kor)
+            .exists()
+        )
+
+        if instance_exists:
+            print("Already crawled member. Terminating process..")
+            return
+
+        name = {"kor": member_kor, "eng": member_eng}
+        member_instance = IdolMember.objects.create(name=name)
+
+        IdolMemberIncluded.objects.create(group_id=group_id, member=member_instance)
+
+        source = {}
+        info = {}
+
+        for node in info_nodes:
+            key = node.select_one("dt").text
+            if key == "출생":
+                birth = node.select_one("dd").text
+                info[key] = birth
+            elif key == "사이트":
+                sources = node.select("dd a")
+                for s in sources:
+                    source[s.text] = s["href"]
+                break
+            else:
+                continue
+
+        image_resource_instance = ImageResource.objects.create(
+            address=thumbnail_address
+        )
+        IdolMemberInfo.objects.create(
+            member=member_instance,
+            thumbnail=image_resource_instance,
+            info=info,
+            source=source,
+        )
