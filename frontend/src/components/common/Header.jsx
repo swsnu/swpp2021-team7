5217b9a6e262223ba95d95d983e94d9a859436b1
eunbin1227 <eunbin1227@snu.ac.kr>
2021-12-08 15:06:09 +0900

    Load Ranking

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -4,6 +4,7 @@ import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import { withRouter } from 'react-router';
 import { useLocation } from 'react-router';
+import axios from 'axios';
 
 const pathName = ["/logout", "/mypage/1", "/"]
 
@@ -20,16 +21,34 @@ function Header(props) {
 
     const location = useLocation();
     const [value, setValue] = React.useState(checkPathName(location.pathname));
+    const [isLogin, setIsLogin] = React.useState(false);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+        getData();
         props.history.push(newValue)
     };
 
+    async function getData() {
+        try {
+          const response = await axios.get('/account/signout/');
+          console.log(response);
+        } catch(err) {
+          console.error(err);
+        }
+    }
 
     React.useEffect(() => {
         setValue(checkPathName(location.pathname));
     }, [location.pathname])
+
+    React.useEffect(() => {
+        async function getLoginStatus() {
+            const response = await axios.get('/account/islogin/').catch({});
+            setIsLogin(response.data['status'])       
+        }
+        getLoginStatus();
+    }, [props])
 
     return (
         <Box sx={{ width: '100%' }}>
@@ -45,7 +64,11 @@ function Header(props) {
                 value={value}
                 aria-label="secondary tabs example"
             >
-                <Tab value="/logout" label="Logout" />
+                {   
+                    isLogin ? 
+                    <Tab value="/sign/login" label="Logout" /> :
+                    <Tab value="/sign/login" label="Login" />
+                }
                 <Tab value="/mypage/1" label="MyPage" />
                 <Tab value="/" label="Main" />
             </Tabs>
