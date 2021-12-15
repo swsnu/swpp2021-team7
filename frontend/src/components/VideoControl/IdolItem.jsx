4c6cf7ea58ccb5ef0a9f93cec1a0a738378af2f5
Sohyun Kim <prrsh22@gmail.com>
2021-12-15 14:17:33 +0900

    Call idol request view

--- frontend/src/components/VideoControl/IdolItem.jsx
+++ frontend/src/components/VideoControl/IdolItem.jsx
@@ -5,10 +5,12 @@ import {
     ListItemText,
     ListItemAvatar,
     Avatar,
-    Chip
+    Chip,
+    Button
 } from '@mui/material';
 import { withRouter } from 'react-router';
 import "./IdolItem.css";
+import axios from 'axios';
 
 class IdolItem extends Component {
     constructor(props){
@@ -18,11 +20,21 @@ class IdolItem extends Component {
         //this.props.history.push('/video/result');
     }
 
+    handleRequestClick = async () => {
+        await axios.post("/search-result/request-support/", {"name": this.props.name})
+        .then(() => {
+            alert(`Successfully submitted. We would make [${this.props.name}] available shortly.`)
+        })
+        .catch((e) => {
+            alert("Excessive requests in short time. Request again after a while.");
+        })
+    }
+
     render() {
         return (
             <div
-                onClick={this.props.onClick}
-                className="idol-items">
+                onClick={this.props.active == "active" ? this.props.onClick : null}
+                className="idol-items" style={{cursor: this.props.active == "active" ? "pointer" : "unset"}}>
                 <ListItem 
                     id="IDOL-ITEM"
                     className={(this.props.active == "active") ? "" : "not-ready"}
@@ -52,10 +64,10 @@ class IdolItem extends Component {
                             }}onClick={()=>this.redirectSearchResult()} clickable label={this.props.name} />}
                     />
                 </ListItem>
-                {(this.props.active == "active") ? 
-                "" : 
-                <p
-                    className="not-ready-p">Not Supported. Would You Request Support?</p>}
+                {(this.props.active !== "active") && <>
+                <p className="not-ready-p">Not Supported. Would You Request Support?</p>
+                <Button variant="outlined" color="error" onClick={this.handleRequestClick}>Request Support</Button>
+                </>}
                 <Divider sx={{marginLeft : 0}} variant="inset" component="li" />
             </div>
         )
