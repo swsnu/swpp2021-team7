bce4f05a1de7d8be974830b38d09110eaea7b903
YCyoon1994 <withtogetherapp@gmail.com>
2021-12-15 15:31:38 +0900

    face recognition

--- backend/ml/analyze/faceRecognition.py
+++ backend/ml/analyze/faceRecognition.py
@@ -1,5 +1,6 @@
 import face_recognition
 import cv2
+import requests
 
 class faceRecognition:
     def __init__(self, video="", idol=[]):
@@ -19,19 +20,32 @@ class faceRecognition:
         if(len(self.idol) == 0):
             print("no targeted idols.")
             return
-        input_movie = cv2.VideoCapture(self.video)
-        length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
 
+        real_fps = 0.1
+        prev_time = 0
+        
+        
+        input_movie = cv2.VideoCapture(self.video)
+        input_movie.set(cv2.CAP_PROP_FPS, 1)
+
+        
+        length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = float(input_movie.get(cv2.CAP_PROP_FPS))
+        time = length / fps
+        
         # Create an output movie file (make sure resolution/frame rate matches input video!)
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
         output_movie = cv2.VideoWriter('output.avi', fourcc, 29.97, (640, 360))
 
         # Load some sample pictures and learn how to recognize them.
         known_faces = []
+        results = []
         for img in self.idol:
-            face_image = face_recognition.load_image_file(img.src)
+            raw_image = requests.get(img, stream=True).raw
+            face_image = face_recognition.load_image_file(raw_image)
             face_encoding = face_recognition.face_encodings(face_image)[0]
             known_faces.append(face_encoding)
+            results.append([])
    
 
         # Initialize some variables
@@ -43,51 +57,57 @@ class faceRecognition:
         while True:
             # Grab a single frame of video
             ret, frame = input_movie.read()
+            
             frame_number += 1
-
+            
             # Quit when the input video file ends
             if not ret:
                 break
+            if frame_number % round(fps / real_fps) == 0:
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_frame = frame[:, :, ::-1]
 
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_frame = frame[:, :, ::-1]
+                # Find all the faces and face encodings in the current frame of video
+                face_locations = face_recognition.face_locations(rgb_frame)
+                face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_frame)
-            face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+                face_names = []
+                
+                print(face_encodings)
+                for face_encoding in face_encodings:
+                    # See if the face is a match for the known face(s)
+                    match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+                    print(face_encoding,match)
+                    # If you had more than 2 faces, you could make this logic a lot prettier
+                    # but I kept it simple for the demo
+                    name = None
+                    index_number = 0
+                    for i in known_faces:
+                        if match[index_number]:
+                            name = self.idol[index_number]
+                            results[index_number].append(frame_number/10)
+                        index_number += 1
+                    face_names.append(name)
+                    
 
-            face_names = []
-            index_number = 0
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+                # Label the results
+                for (top, right, bottom, left), name in zip(face_locations, face_names):
+                    if not name:
+                        continue
 
-                # If you had more than 2 faces, you could make this logic a lot prettier
-                # but I kept it simple for the demo
-                name = None
-                if match[index_number]:
-                    name = self.idol[index_number]
+                    # Draw a box around the face
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-                face_names.append(name)
-                index_number += 1
+                    # Draw a label with a name below the face
+                    cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
 
-            # Label the results
-            for (top, right, bottom, left), name in zip(face_locations, face_names):
-                if not name:
-                    continue
-
-                # Draw a box around the face
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-                # Draw a label with a name below the face
-                cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
-
-            # Write the resulting image to the output video file
-            print("Writing frame {} / {}".format(frame_number, length))
-            output_movie.write(frame)
+                # Write the resulting image to the output video file
+                print("Writing frame {} / {}".format(frame_number, length))
+                #output_movie.write(frame)
 
         # All done!
         input_movie.release()
         cv2.destroyAllWindows()
+        return results
