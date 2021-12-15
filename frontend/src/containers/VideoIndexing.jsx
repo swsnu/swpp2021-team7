85780c8df39854051c74116204bacc7ad506b2b2
YCyoon1994 <withtogetherapp@gmail.com>
2021-12-15 12:36:17 +0900

    video parameter

--- frontend/src/containers/VideoIndexing.jsx
+++ frontend/src/containers/VideoIndexing.jsx
@@ -136,6 +136,7 @@ class VideoIndexing extends Component {
                 )}
                 <Container maxWidth="sm">
                     <ChooseType
+                        urlString={this.state.url}
                         isView={(this.state.url)? true : false}></ChooseType>
                 </Container>
             </React.Fragment>
