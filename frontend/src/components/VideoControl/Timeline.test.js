677e0553ed9747bd45f3c3ffc8bbbe7cc0fd01cb
ParkJiHo <iks15174@daum.net>
2021-12-16 04:17:06 +0900

    add front end test & remove action folder

--- frontend/src/components/VideoControl/Timeline.test.js
+++ frontend/src/components/VideoControl/Timeline.test.js
@@ -11,7 +11,7 @@ const mockStore = getMockStore({});
 
 describe('<Timeline />', () => {
     let component = null
-    const TEST = [12,34,123,40,102,30,12,34,65,76,10,32,67,90];
+    const TEST = [12, 34, 123, 40, 102, 30, 12, 34, 65, 76, 10, 32, 67, 90];
     const FaceRecognition = 1002;
     const CutScene = 1001;
     let setComponent = () => {
@@ -25,17 +25,6 @@ describe('<Timeline />', () => {
             </Provider>
         )
     }
-    it('should render without errors', () => {
-        setComponent()
-        const container = component.find('.timeline-container')
-        const activeBox = component.find('.active-box');
-        const deactiveBox = component.find('.deactive-box');
-        
-        expect(container.length).toBe(1);
-        expect(activeBox.length).toBe(0);
-        expect(deactiveBox.length).toBe(0);
-
-    })
     let setComponent2 = () => {
         component = mount(
             <Provider store={mockStore} >
@@ -47,12 +36,23 @@ describe('<Timeline />', () => {
             </Provider>
         )
     }
+    it('should render without errors', () => {
+        setComponent()
+        const container = component.find('.timeline-container')
+        const activeBox = component.find('.active-box');
+        const deactiveBox = component.find('.deactive-box');
+
+        expect(container.length).toBe(1);
+        expect(activeBox.length).toBe(7);
+        expect(deactiveBox.length).toBe(7);
+
+    })
     it('should render without errors for cutscene', () => {
         setComponent2()
         const container = component.find('.timeline-container')
         const activeBox = component.find('.active-box');
         const deactiveBox = component.find('.deactive-box');
-        
+
         expect(container.length).toBe(1);
         expect(activeBox.length).toBe(7);
         expect(deactiveBox.length).toBe(7);
